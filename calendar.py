3bab80a3dfb0add064d3f682706453fd62dae275
Kevin Wangome Kinyua <w.kevinkinyua@gmail.com>
2017-09-24 16:21:50 +0300

    Rename calendar to calendar.py

---
+++ calendar.py
@@ -0,0 +1,11 @@
+# Python program to display  of given month of the year
+
+# import module
+import 
+
+# To ask month and year from the user
+yy = int(input("Enter year: "))
+mm = int(input("Enter month: "))
+
+# display the 
+print(.month(yy, mm))
